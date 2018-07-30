359b6ceb05f7556d38547ec7ab4dbf9a1bff780d
eudalio <tonnydallios@gmail.com>
2018-07-30 15:12:33 -0300

    Instalando styled-components

--- modulo2_app_github_compare/src/App.js
+++ modulo2_app_github_compare/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './styles/global';
+
 const App = () => (
   <div className="App">
     <h1 className="App-title">Welcome to React</h1>
   </div>
-)
+);
 
 export default App;
